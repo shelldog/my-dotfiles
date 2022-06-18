8e3cc0240d3861647c78c2c0cf421e2532bcbab9
shelldog <hajimedevlife@gmail.com>
2022-06-18 11:33:19 +0700

    first push!

---
+++ fish/conf.d/fish_color.fish
@@ -0,0 +1,9 @@
+# set color command.
+set fish_color_command CCCCCC
+set fish_color_error 004070
+
+# Set color for the prompt.
+set color_user_bg 303030 
+set color_user_str B8B8B8
+set color_dir_bg 1B1B24 
+set color_dir_str white
---
+++ fish/conf.d/tide.fish
@@ -0,0 +1,48 @@
+# git.
+set -g tide_git_bg_color 268bd2
+set -g tide_git_bg_color_unstable C4A000
+set -g tide_git_bg_color_urgent CC0000
+set -g tide_git_branch_color 000000
+set -g tide_git_color_branch 000000
+set -g tide_git_color_conflicted 000000
+set -g tide_git_color_dirty 9d2933
+set -g tide_git_color_operation 000000
+set -g tide_git_color_staged 000000
+set -g tide_git_color_stash 000000
+set -g tide_git_color_untracked 000000
+set -g tide_git_color_upstream 000000
+set -g tide_git_conflicted_color 000000
+set -g tide_git_dirty_color 000000
+set -g tide_git_icon 
+set -g tide_git_operation_color 000000
+set -g tide_git_staged_color 000000
+set -g tide_git_stash_color 000000
+set -g tide_git_untracked_color 000000
+set -g tide_git_upstream_color 000000
+
+# pwd.
+set -g tide_pwd_bg_color 444444
+set -g tide_pwd_color_anchors efefef
+set -g tide_pwd_color_dirs bbbbbc
+set -g tide_pwd_color_truncated_dirs efefef
+
+# node.
+set -g tide_node_bg_color 32A852
+set -g tide_node_color 000000 
+
+# time.
+set -g tide_time_bg_color c3c6bc
+set -g tide_time_color 000000
+
+# status.
+set -g tide_status_bg_color 2e7527
+set -g tide_status_icon '\u2713'
+
+# prompt.
+set -g tide_prompt_color_frame_and_connection 444444
+set -g tide_left_prompt_separator_diff_color ''
+set -g tide_right_prompt_separator_diff_color ''
+
+# cmd_duration.
+set -g tide_cmd_duration_bg_color C4A000 
+set -g tide_cmd_duration_color 000000
